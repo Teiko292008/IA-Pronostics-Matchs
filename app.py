7dd68e821c2f475072df84a0d6a0b239d89696ea
Teiko292008 <tlr292008@gmail.com>
2025-07-18 11:43:14 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="IA Pronos", page_icon="⚽")
+st.title("🧠 IA – Meilleurs Pronostics Sportifs")
+st.write("Bienvenue dans ton app IA de pronostics sportifs ! 🔥")
